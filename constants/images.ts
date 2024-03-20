56432e323a6d964f2f7acdfbe8dca81c9db502af
Joel-Ralaison <hery.ralaison@gmail.com>
2024-03-20 23:56:13 +0300

    optimisation: image files

--- constants/images.ts
+++ constants/images.ts
@@ -1,7 +1,7 @@
 import logo from "/public/assets/logo.png";
-import bgHome from "/public/assets/bg.jpg";
-import img1 from "/public/assets/img-1.jpg";
-import img2 from "/public/assets/img-2.jpg";
-import img3 from "/public/assets/loginBg.jpg";
+import bgHome from "/public/assets/bg.webp";
+import img1 from "/public/assets/img-1.webp";
+import img2 from "/public/assets/img-2.webp";
+import img3 from "/public/assets/loginBg.webp";
 
 export { logo, bgHome, img1, img2, img3 };
